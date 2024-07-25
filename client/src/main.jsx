d991979f158731a7620a0e31e7d054820c7eeca6
Loic legoff <loic95280@gmail.com>
2024-07-25 09:54:14 +0200

    first commit loic

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,11 @@
+// Importation des modules nécessaires de React et ReactDOM
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// Importation des fonctions nécessaires pour la gestion des routes depuis react-router-dom
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Importation des composants utilisés dans les routes
 import App from "./App";
 import Menu from "./pages/menuPage";
 import Connexion from "./components/connexion/connexion";
@@ -9,10 +14,13 @@ import Continents from "./components/accueil/Accueil";
 import Favoris from "./components/favoris/fav";
 import Admin from "./components/admin/Admin";
 
+// Création du routeur en utilisant createBrowserRouter avec une configuration de routes
 const router = createBrowserRouter([
   {
+    // Composant principal qui sera le conteneur pour les routes enfants
     element: <App />,
 
+    // Définition des routes enfants
     children: [
       {
         path: "/",
@@ -42,8 +50,10 @@ const router = createBrowserRouter([
   },
 ]);
 
+// Création de la racine de l'application React dans l'élément avec l'ID "root"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// Rendu de l'application en mode strict avec le RouterProvider qui utilise le routeur configuré
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
